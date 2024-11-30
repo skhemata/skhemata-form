1b277bbebd07e41cbbe0b0a6463d3722a623c784
Ryan L <ryan.lac@devradius.com>
2024-11-29 16:10:44 -0800

    check if defined

--- skhemata-form.ts
+++ skhemata-form.ts
@@ -12,21 +12,48 @@ import { SkhemataFormRepeat } from './src/SkhemataFormRepeat';
 import { SkhemataFormDatePicker } from './src/SkhemataFormDatePicker';
 import { SkhemataFormTime } from './src/SkhemataFormTime';
 
-
-window.customElements.define('skhemata-form', SkhemataForm);
-window.customElements.define('skhemata-form-textbox', SkhemataFormTextbox);
-window.customElements.define('skhemata-form-textarea', SkhemataFormTextarea);
-window.customElements.define('skhemata-form-checkbox', SkhemataFormCheckbox);
-window.customElements.define('skhemata-form-toggle', SkhemataFormToggle);
-window.customElements.define('skhemata-form-repeat', SkhemataFormRepeat);
-window.customElements.define('skhemata-form-date-picker', SkhemataFormDatePicker);
-window.customElements.define('skhemata-form-time', SkhemataFormTime);
-
-window.customElements.define('skhemata-form-dropdown', SkhemataFormDropdown);
-window.customElements.define('skhemata-form-dropzone', SkhemataFormDropzone);
-window.customElements.define('skhemata-form-quill', SkhemataFormQuill);
-window.customElements.define('skhemata-form-button', SkhemataFormButton);
-window.customElements.define(
-  'skhemata-form-autocomplete',
-  SkhemataFormAutocomplete
-);
+if (!customElements.get('skhemata-form')) {
+  window.customElements.define('skhemata-form', SkhemataForm);
+}
+if (!customElements.get('skhemata-form-textbox')) {
+  window.customElements.define('skhemata-form-textbox', SkhemataFormTextbox);
+}
+if (!customElements.get('skhemata-form-textarea')) {
+  window.customElements.define('skhemata-form-textarea', SkhemataFormTextarea);
+}
+if (!customElements.get('skhemata-form-checkbox')) {
+  window.customElements.define('skhemata-form-checkbox', SkhemataFormCheckbox);
+}
+if (!customElements.get('skhemata-form-toggle')) {
+  window.customElements.define('skhemata-form-toggle', SkhemataFormToggle);
+}
+if (!customElements.get('skhemata-form-repeat')) {
+  window.customElements.define('skhemata-form-repeat', SkhemataFormRepeat);
+}
+if (!customElements.get('skhemata-form-date')) {
+  window.customElements.define(
+    'skhemata-form-date-picker',
+    SkhemataFormDatePicker
+  );
+}
+if (!customElements.get('skhemata-form-time')) {
+  window.customElements.define('skhemata-form-time', SkhemataFormTime);
+}
+if (!customElements.get('skhemata-form-dropdown')) {
+  window.customElements.define('skhemata-form-dropdown', SkhemataFormDropdown);
+}
+if (!customElements.get('skhemata-form-dropzone')) {
+  window.customElements.define('skhemata-form-dropzone', SkhemataFormDropzone);
+}
+if (!customElements.get('skhemata-form-quill')) {
+  window.customElements.define('skhemata-form-quill', SkhemataFormQuill);
+}
+if (!customElements.get('skhemata-form-button')) {
+  window.customElements.define('skhemata-form-button', SkhemataFormButton);
+}
+if (!customElements.get('skhemata-form-autocomplete')) {
+  window.customElements.define(
+    'skhemata-form-autocomplete',
+    SkhemataFormAutocomplete
+  );
+}
